451d39066c15ac196e91bbe788fa183acc3f6155
Nikita517 <67646382+Nikita517@users.noreply.github.com>
2020-08-10 11:40:00 +0300

    Update style.js

--- src/calendar/style.js
+++ src/calendar/style.js
@@ -1,5 +1,6 @@
 import {StyleSheet} from 'react-native';
 import * as defaultStyle from '../style';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const STYLESHEET_ID = 'stylesheet.calendar.main';
 
@@ -15,12 +16,11 @@ export default function getStyle(theme={}) {
       backgroundColor: appStyle.calendarBackground
     },
     week: {
-      marginTop: 7,
-      marginBottom: 7,
+      marginTop: hp('1.6%'),
+      marginBottom: hp('1.6%'),
       flexDirection: 'row',
       justifyContent: 'space-around'
     },
     ...(theme[STYLESHEET_ID] || {})
   });
 }
-
